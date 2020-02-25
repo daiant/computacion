8deb67c5c1ae7638acf45898a5e12b087e60ad2a
Daiant <cmg2512@gmail.com>
2020-02-25 12:58:56 +0100

    cosas varias

---
+++ AIN/Practicas Laboratorio/Sesion1_ejemplos/robot.asl
@@ -0,0 +1,41 @@
+/* Beliefs */
+adyacente(a, b).
+adyacente(b, c).
+adyacente(c, d).
+adyacente(d, a).
+localizado(robot, b).
+localizado(papel_usado, a).
+localizado(papelera, d).
+localizado(papel_usado, c).
+papeles(2).
+
+
+/* Goals */
+!localizado(robot, d).
+
+/* Events */
++localizado(robot, X) : localizado(papel_usado, X)
+			<- .print("papel cogido en ", X);
+			   -localizado(papel_usado, X);
+			   +llevando(papel_usado);
+				 -papeles(P);
+				 +papeles(P-1).
+
+
++localizado(robot, X) : localizado(papelera, X) &
+				llevando(papel_usado)
+			<- .print("papel tirado a la papelera en ", X).
+
++!localizado(robot, X) : localizado(robot, X) & papeles(0) <- .print("Ha llegado a su destino").
++!localizado(robot, X) : localizado(robot, X) & papeles(P) & (not (P=0)) & adyacente(X,Z)
+<- .print("mover de ", X, " a ", Z);
+	 -localizado(robot, Y);
+	 +localizado(robot, Z);
+	 !localizado(robot, X).
+
++!localizado(robot, X) : localizado(robot, Y) &
+				(not (X=Y)) & adyacente(Y,Z)
+			<- .print("mover de ", Y, " a ", Z);
+			   -localizado(robot, Y);
+			   +localizado(robot, Z);
+			   !localizado(robot, X).
---
+++ SAR/Practicas/P1.- Pig Latin/SAR_p1_piglatin_plantilla.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+#! -*- encoding: utf8 -*-
+
+# 1.- Pig Latin
+
+import sys
+import re
+
+
+class Translator():
+
+    def __init__(self, punt=None):
+        """
+        Constructor de la clase Translator
+
+        :param punt(opcional): una cadena con los signos de puntuación
+                                que se deben respetar
+        :return: el objeto de tipo Translator
+        """
+        if punt is None:
+            self.re = re.compile("(\w+)([.,;?!]*)")
+        else:
+            self.re = re.compile("(\w+)(["+punt+"]*)")
+
+    def translate_word(self, word):
+        """
+        Este método recibe una palabra en inglés y la traduce a Pig Latin
+
+        :param word: la palabra que se debe pasar a Pig Latin
+        :return: la palabra traducida
+        """
+        vocalic = ['a','e','i','o','u', 'y', 'A', 'E', 'I', 'O', 'U', 'Y']
+        firstCapital = False
+        word1, s= self.re.match(word).groups()
+        res = ""
+        if(word1[0].isnumeric()):
+            return word
+        if(word1[0] in vocalic):
+            yay = "YAY" if word1.isupper() else "yay"
+            return word1+yay+s
+        else:
+            if(word1[0].isupper() and not word1.isupper()):
+                word1 = word1[0].lower()+word1[1:]
+                firstCapital = True
+            while(len(word1)>0):
+                if(word1[0] not in vocalic):
+                    res += word1[0]
+                    word1 = word1[1:]
+                else:
+                    ay = "AY" if word1.isupper() else "ay"
+                    res += ay
+                    res = word1+res
+                    break
+            if(firstCapital):
+                res=res[0].upper()+res[1:]
+        return res+s
+
+    def translate_sentence(self, sentence):
+        """
+        Este método recibe una frase en inglés y la traduce a Pig Latin
+
+        :param sentence: la frase que se debe pasar a Pig Latin
+        :return: la frase traducida
+        """
+
+        # sustituir
+        new_sentence = ""
+        for word in sentence.split():
+            new_sentence += self.translate_word(word) + " "
+
+        return new_sentence
+
+    def translate_file(self, filename):
+        """
+        Este método recibe un fichero y crea otro con su tradución a Pig Latin
+
+        :param filename: el nombre del fichero que se debe traducir
+        :return: True / False
+        """
+
+        # rellenar
+        fp = open(filename, 'r')
+        out = open(filename.split(".")[0] + "_latin.txt" , 'w')
+        for line in fp:
+            out.write(self.translate_sentence(line).strip()+'\n')
+        out.close()
+        fp.close()
+
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        print('Syntax: python %s [filename]' % sys.argv[0])
+        exit
+    else:
+        t = Translator()
+        if len(sys.argv) == 2:
+            t.translate_file(sys.argv[1])
+        else:
+            while True:
+                sentence = input("ENGLISH: ")
+                if len(sentence) < 2:
+                    break
+                print("PIG LATIN:", t.translate_sentence(sentence))
